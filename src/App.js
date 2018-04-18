03f867ec752769b0bded982fddafc37871ce762b
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-18 20:49:26 +0700

    ListWord

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { StateExam } from './components/screens/StateExam';
+// import { StateExam } from './components/screens/StateExam';
+import { ListWord } from './components/screens/ListWord';
 
 class App extends Component {
   render() {
-    return <StateExam />
+    return <ListWord />
   }
 }
 
--- src/components/screens/ListWord.js
+++ src/components/screens/ListWord.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import { Word } from '../shared/Word';
 
 export class ListWord extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            words: [
+                { _id: 'a', en: 'One', vn: 'Mot', isMemorized: true },
+                { _id: 'b', en: 'Two', vn: 'Hai', isMemorized: false },
+                { _id: 'c', en: 'Three', vn: 'Ba', isMemorized: true },
+            ]
+        };
+    }
     genWord(word) {
         return (
             <div key={word._id}>
@@ -12,14 +22,9 @@ export class ListWord extends Component {
     }
 
     render() {
-        const arr = [
-            { _id: 'a', en: 'One', vn: 'Mot', isMemorized: true },
-            { _id: 'b', en: 'Two', vn: 'Hai', isMemorized: false },
-            { _id: 'c', en: 'Three', vn: 'Ba', isMemorized: true },
-        ]
         return (
             <div>
-                { arr.map(word => <Word wordInfo={word} key={word._id} />) }
+                { this.state.words.map(this.genWord) }
             </div>
         );
     }
