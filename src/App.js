9a3fddade4f01cf535f2d7f15ccd4e02d3f21ff5
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-18 20:30:34 +0700

    lap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React, { Component } from 'react';
-// import { Sample } from './components/screens/Sample';
+import { ListWord } from './components/screens/ListWord';
 import { Word } from './components/shared/Word';
 
 class App extends Component {
   render() {
-    const word1 = { en: 'One', vn: 'Mot', isMemorized: true };
-    const word2 = { en: 'Two', vn: 'Hai', isMemorized: false };
-    const word3 = { en: 'Three', vn: 'Ba', isMemorized: true };
-    return (
-      <div className="App">
-        <Word wordInfo={word1} />
-        <Word wordInfo={word2} />
-        <Word wordInfo={word3} />
-      </div>
-    );
+    return <ListWord />
   }
 }
 
