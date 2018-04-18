f0172d5b831b67bcc15fe2bcc4091cef597b59e6
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-18 20:59:31 +0700

    remove

--- src/components/screens/ListWord.js
+++ src/components/screens/ListWord.js
@@ -11,12 +11,26 @@ export class ListWord extends Component {
                 { _id: 'c', en: 'Three', vn: 'Ba', isMemorized: true },
             ]
         };
+        this.genWord = this.genWord.bind(this);
     }
+
+    removeWord(_id) {
+        this.setState(prevState => ({
+            words: prevState.words.filter(w => w._id !== _id)
+        }));
+    }
+
     genWord(word) {
         return (
             <div key={word._id}>
                 <h3>{word.en}</h3>
                 <p>{word.vn}</p>
+                <button
+                    className="btn btn-danger"
+                    onClick={() => this.removeWord(word._id)}
+                >
+                    Remove
+                </button>
             </div>
         );
     }
